7718f813b6c1478ad963d4dd1c6a49b7294969af
Timothy Fawcett <tim.fawcett89@gmail.com>
2023-02-10 20:47:06 +1100

    added domain regex

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const axios = require('axios')
 require('dotenv').config()
+const tldjs = require('tldjs')
+const { check, validationResult } = require('express-validator');
 
 
 const port = process.env.PORT || 5000
@@ -15,10 +17,12 @@ app.use(express.urlencoded({ extended: true }))
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.header(
+    "Access-Control-Allow-Methods",
+    "GET,POST"
+  )
+  res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept",
-    
-  );
+    "Origin, X-Requested-With, Content-Type, Accept",);
 
 
   next();
@@ -31,15 +35,34 @@ app.get('/user', async (req, res) => {
 
 })
 
+
+
+const DOMAIN_REGEX = /^(?!:\/\/)([a-zA-Z0-9-_]+\.)*[a-zA-Z0-9][a-zA-Z0-9-_]+\.[a-zA-Z]{2,11}?$/;
+
 app.post('/search', async (req, res) => {
-  const searchIp = await req.body.values.ipAddress
+  const value = await req.body.values.inputValue
 
+    let search_fetch_response = {}
 
-  const search_fetch_response = await
-    axios.get(`${url}apiKey=${apiKey}&ipAddress=${searchIp}`);
-  
-  
-  const searchIpData = await search_fetch_response.data;
+    
+  if (DOMAIN_REGEX) {
+    try{  
+      search_fetch_response = await
+        axios.get(`${url}apiKey=${apiKey}&domain=${value}`);
+    } catch (error) {
+      return res.status(500).send({ error: error.message });
+    }
+  }
+//   else {
+//     try {
+//         search_fetch_response = await
+//         axios.get(`${url}apiKey=${apiKey}&ipAddress=${value}`);
+//     } catch (error ) {
+//       return res.status(500).send({ error: error.message });
+//     }
+// } 
+
+  let searchIpData = await search_fetch_response.data;
   res.json(searchIpData)
   console.log(searchIpData)
 })
